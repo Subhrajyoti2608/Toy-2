a8e11234e0c4442d42e1c7b6143676ce506d7c65
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-02-28 16:05:44 +0530

    Update eventListener.js

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,8 +1,18 @@
+var uid = null;
+
 AFRAME.registerComponent("marker-handler",{
 init: async function(){
+
+
+  if(uid === null){
+    this.askuid()
+  }
 this.el.addEventListener("markerFound", ()=>{
+  if(uid !== null){
 console.log("marker is found!")
   this.handleMarkerFound()
+
+  }
 })
   
 this.el.addEventListener("markerLost", ()=>{
@@ -11,43 +21,126 @@ console.log("marker is lost!")
 })
 },
   
-handleMarkerFound: function(){
-var buttonDiv = document.getElementById("button-div")
+askuid: function(){
 
-buttonDiv.style.display = "flex"
-  
-var ratingButton = document.getElementById("summary")
+  var iconurl = "toy.jpg"
 
-var orderButton = document.getElementById("order")
-
-ratingButton.addEventListener("click", function(){
   swal({
-  icon:"warning",
-  title:"rate toy",
-  text:"work in progress"
+    title:"",
+    icon:iconurl,
+    content:{
+      element:"input",
+      attributes:{
+        placeholder: "type your uid Ex.(U01)",
+        type: "string",
+        min: 1
+      }
+    },
+    closeOnClickOutside: false
+  }).then(inputValue =>{
+    uid = inputValue
   })
-})
-  
-  orderButton.addEventListener("click", function(){
-  swal({
-  icon:"warning",
-  title:"Thanks for ordering!",
-  text:"It will be delivered soon!"
-  })
-})
-
-var toy = toys.filter(toy => toy.id === markerId)[0]
-
-var model = document.querySelector(`#model-${toy.id}`)
-model.setAttribute("position",toy.model_geometry.position)
-model.setAttribute("rotation",toy.model_geometry.rotation)
-model.setAttribute("scale",toy.model_geometry.scale)
 },
-  
- handleMarkerLost: function(){
- var buttonDiv = document.getElementById("button-div")
 
- buttonDiv.style.display = "none";
+handleMarkerFound: function(toys, markerId){
+
+   // Getting today's day
+   var todaysDate = new Date();
+   var todaysDay = todaysDate.getDay();
+   
+   // Sunday - Saturday : 0 - 6
+   var days = [
+     "sunday",
+     "monday",
+     "tuesday",
+     "wednesday",
+     "thursday",
+     "friday",
+     "saturday"
+   ];
+
+   var toy = toys.filter(toy => toys.filter === markerId)[0]
+
+   if (toy.unavailable_days.includes(days[todaysDay])) {
+    swal({
+      icon: "warning",
+      title: toy.toy_name.toUpperCase(),
+      text: "This toy is not available today!!!",
+      timer: 2500,
+      buttons: false
+    });
+  } else {
+     // Changing Model scale to initial scale
+    var model = document.querySelector(`#model-${toy.id}`);
+    model.setAttribute("position", toy.model_geometry.position);
+    model.setAttribute("rotation", toy.model_geometry.rotation);
+    model.setAttribute("scale", toy.model_geometry.scale);
+
+    //Update UI conent VISIBILITY of AR scene(MODEL , INGREDIENTS & PRICE)
+
+    model.setAttribute("visible", true);
+    
+
+    var ingredientsContainer = document.querySelector(`#main-plane-${toy.id}`);
+
+    ingredientsContainer.setAttribute("visible", true);
+
+    var pricePlane = document.querySelector(`#price-plane-${toy.id}`);
+
+    pricePlane.setAttribute("visible", true);
+
+
+        var buttonDiv = document.getElementById("button-div")
+
+        buttonDiv.style.display = "flex"
+          
+        var ratingButton = document.getElementById("summary")
+
+        var orderButton = document.getElementById("order")
+
+        ratingButton.addEventListener("click", function(){
+          swal({
+          icon:"warning",
+          title:"rate toy",
+          text:"work in progress"
+          })
+        })
+        
+        orderButton.addEventListener("click", function(){
+        swal({
+        icon:"warning",
+        title:"Thanks for ordering!",
+        text:"It will be delivered soon!"
+        })
+      })
+
+    }
+
+    
+          
+        
+ },
+
+ handleOrder: function(tNumber, toy) {
+  firebase.firestore().collection("tables").doc(tNumber).get().then((doc) =>{
+   var details = doc.data()
+
+   if(details["current_orders"][toy.id]){
+     details["current_orders"][toy.id]["quantity"]+=1
+     var currentQuantity = details["current_orders"][toy.id]["quantity"]
+     details["current_orders"][toy.id]["subtotal"]= currentQuantity*toy.price
+   }else{
+     details["current_orders"][toy.id]={
+       item: toy.toy_name,
+       price: toy.price,
+       quantity: 1,
+       subtotal: toy.price*1
+     }
+   }
+
+   details.total_bill += toy.price
+   firebase.firestore().collection("users").doc(doc.id).update(details)
+  })
  },
 
  getToys: async function(){
@@ -58,5 +151,12 @@ model.setAttribute("scale",toy.model_geometry.scale)
   .then(snap => {
     return snap.docs.map(doc => doc.data())
   })
- }
+ },
+
+ handleMarkerLost: function(){
+  var buttonDiv = document.getElementById("button-div")
+
+  buttonDiv.style.display = "none";
+}   
+ 
 })
